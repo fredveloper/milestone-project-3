04bb83786aac6dc959d8f2f4b23fc26273b3c677
Jose Mota <fredveloper@gmail.com>
2020-06-19 23:05:50 +0000

    Created categories.html and editcategory.html templates. Added get_categories, edit_category, update_category and delete_category routes to app.py file. Added cancel button to editrecipe.html template

--- app.py
+++ app.py
@@ -21,10 +21,12 @@ def get_recipes():
     recipe_list = [recipe for recipe in _recipes]
     return render_template('recipes.html', recipes = recipe_list)
 
+
 @app.route('/add_recipe')
 def add_recipe():
     return render_template('addrecipe.html',
     categories=mongo.db.categories.find())   
+
 
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
@@ -32,11 +34,13 @@ def insert_recipe():
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
 
+
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
     the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     all_categories = mongo.db.categories.find()
     return render_template('editrecipe.html', recipe=the_recipe, categories=all_categories)
+
 
 @app.route('/update_recipe/<recipe_id>', methods=["POST"])
 def update_recipe(recipe_id):
@@ -53,10 +57,38 @@ def update_recipe(recipe_id):
     })
     return redirect(url_for('get_recipes'))
 
+
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
     return redirect(url_for('get_recipes'))
+
+#----------Categories----------
+
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html',
+    categories=mongo.db.categories.find())
+
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+    category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
 
 
 if __name__ == '__main__':
